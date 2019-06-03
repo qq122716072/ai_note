28b39cc481e5bed2d4a1ae80ba3ddd2306fb7776
qq122716072 <wwzh122716072@163.com>
2019-06-03 10:37:42 +0800

    qq消息保存mysql

--- qq_message/py_mysql.py
+++ qq_message/py_mysql.py
@@ -4,12 +4,19 @@ import time
 import datetime
 
 # 特殊字符集
-apecial_characters_list = ('🐯', '🇨🇳', '🇩🇪', '🦇', '🎸', '🏀', '🐰', '👿', '🤪', '🐳', '💕')
-
-qq_message_director = 'E:/software/qq/qq/data/process/ML  ApacheCN - 副本.txt'
-# qq_message_director = 'E:/software/qq/qq/data/process/hsweb企业后台基础框架.txt'
-group_id = 'hsweb企业后台基础框架'
-group_name = 'hsweb企业后台基础框架'
+apecial_characters_list = ('🐯', '🇨🇳', '🇩🇪', '🦇', '🎸', '🏀', '🐰', '👿', '🤪', '🐳', '💕', '😏', '♨️💯', '👦', '🐮', '🌿', '🤓', '🍀', '🐱', '🚲', '🍀', '🔅', '🍏', '🍎', '🍉', '🍌', '🍒', '🍋',
+                           '🍊', '💋', '　', '🔝', '™', '💎', '🐷', '🌸', '🔥', '🐇', '🐾', '🎈', '🌱', '💨', '🍃', '●', '🐰', '🐒', '💄', '👽', '🐎', '🌈', '🕴', '🇭🇰', '🙈', '🍭', '🐠', '🥟', '👧',
+                           '🌟', '👑', '✨', '🌙', '🏞', '💪', '👈', '👉', '👆', '👇', '💦', '🐶', '🌠', '🏻', '๑', '∀', '🎏', '🌸', '🍺', '🍻', '🧀', '🌃', '☛', 'დ', '☚', '😯', '😡', '🐜', '👀',
+                           '👐', '👏', '🎶', '♛', '🍩', '🙃', '🦄', '😋', '🔫', '🍂', '🤖', '👼', '🐲', '🐉', '🌪','💍', '🅥', '🧐', '🍅', '💗', '', '🌴', '😽', '🌹', '🎀', '️', '️', '✌', '🐥',
+                           '🐤', '💛', '🧘', '😻', '🤩', '🐻', '🚀', '🍁', '🚶', '🛐', '🙄', '󠀀', '🌻', '🤡', '😨', '💅', '🐹', '👠', '❤', '😶', '🌜', '🌛', '🎗', '🍟', '🕊', '👣', '😘', '👘', '🙏',
+                           '𓆡', '𓆝', '𓆟', '𓆜', '👾', '🌺', '💫', '💝', '🍥')
+base = "G:/data/qq/data"
+# base = "E:/software/qq/qq/data"
+fileName = '根根的窗边'
+qq_message_director = base + '/process/' + fileName + '.txt'
+# qq_message_director = base + '/process/SO JSON官方交流①群.txt'
+group_id = fileName
+group_name = group_id
 
 db = pymysql.connect(host = "localhost", user = "root", password = "root", db = "qq_message", port = 3306)
 cur = db.cursor()
@@ -20,7 +27,7 @@ sql_insert_qq_account = 'insert into qq_account (id, qq_id, qq_nick_name) values
 sql_insert_group_account = 'insert into group_account (id, group_id, group_name) values (%s, %s, %s)'
 sql_insert_qq_group_relationship = 'insert into qq_group_relationship (id, qq_id, group_id) values (%s, %s, %s)'
 sql_insert_qq_group_message_relationship = 'insert into qq_group_message_relationship (id, qq_id, group_id, message_id) values (%s, %s, %s, %s)'
-sql_insert_message = 'insert into message (id, message_id, message_receive_date, message_context) values (%s, %s, %s, %s)'
+sql_insert_message = 'insert into message (id, qq_id, qq_nick_name, group_id, group_name, message_id, import_date, message_receive_date, message_context) values (%s, %s, %s, %s, %s, %s, %s, %s, %s)'
 
 
 # start = time.clock()
@@ -28,7 +35,7 @@ start = datetime.datetime.now()
 with open(qq_message_director, 'r', encoding='utf-8', ) as f:
 
     try:
-        cur.execute(sql_insert_group_account, (''.join(str(uuid.uuid4()).split('-')), group_id, group_name))
+        # cur.execute(sql_insert_group_account, (''.join(str(uuid.uuid4()).split('-')), group_id, group_name))
         for line in f:
             # 如果昵称里面有空格不处理
             arr = line.strip().split(' ')
@@ -48,20 +55,20 @@ with open(qq_message_director, 'r', encoding='utf-8', ) as f:
             # 处理qq_id不是(),而是<>
             qq_nick_name = ''
             qq_id = ''
-            if qq_nick_name_and_qq_id.find('<') >= 0 and qq_nick_name_and_qq_id.find('>') >= 0:
-                qq_nick_name, qq_id = qq_nick_name_and_qq_id.split('<')
-                qq_id, _ = qq_id.strip().split('>')
-            if qq_nick_name_and_qq_id.find('(') >= 0 and qq_nick_name_and_qq_id.find(')') >= 0:
-                if qq_nick_name_and_qq_id.find('(') > 0 or qq_nick_name_and_qq_id.find(')') > 0:
-                    print('昵称包含()符号:' + qq_nick_name_and_qq_id)
-                    continue
-                else:
-                    qq_nick_name, qq_id = qq_nick_name_and_qq_id.split('(')
-                    qq_id, _ = qq_id.strip().split(')')
+
+            if qq_nick_name_and_qq_id.endswith('>'):
+                s = qq_nick_name_and_qq_id.rfind('<')
+                e = qq_nick_name_and_qq_id.rfind('>')
+                qq_id = qq_nick_name_and_qq_id[s+1:e]
+                qq_nick_name = qq_nick_name_and_qq_id[:s]
+            if qq_nick_name_and_qq_id.endswith(')'):
+                s = qq_nick_name_and_qq_id.rfind('(')
+                e = qq_nick_name_and_qq_id.rfind(')')
+                qq_id = qq_nick_name_and_qq_id[s+1:e]
+                qq_nick_name = qq_nick_name_and_qq_id[:s]
             message_reveice_data = date + ' ' + time
             if len(qq_nick_name) == 0:
                 qq_nick_name = '-'
-            print(qq_id + '-' + qq_nick_name)
             for e in apecial_characters_list:
                 if qq_nick_name.find(e) >= 0:
                     print("qq昵称存在特殊字符:" + qq_nick_name)
@@ -70,17 +77,18 @@ with open(qq_message_director, 'r', encoding='utf-8', ) as f:
             if (flag == 1):
                 flag = 0
                 continue
-            cur.execute(sql_insert_qq_account, (''.join(str(uuid.uuid4()).split('-')), qq_id, qq_nick_name))
-            cur.execute(sql_insert_qq_group_relationship, (''.join(str(uuid.uuid4()).split('-')), qq_id, group_id))
+            # cur.execute(sql_insert_qq_account, (''.join(str(uuid.uuid4()).split('-')), qq_id, qq_nick_name))
+            # cur.execute(sql_insert_qq_group_relationship, (''.join(str(uuid.uuid4()).split('-')), qq_id, group_id))
             message_id = ''.join(str(uuid.uuid4()).split('-'))
-            print(qq_id + '-' + group_id + '-' + message_id + '-' + message_context)
+            import_date = datetime.datetime.now()
+            cur.execute(sql_insert_message, (''.join(str(uuid.uuid4()).split('-')), qq_id, qq_nick_name, group_id, group_name, message_id, import_date, message_reveice_data, message_context))
+            # cur.execute(sql_insert_qq_group_message_relationship, (''.join(str(uuid.uuid4()).split('-')), qq_id, group_id, message_id))
 
-            cur.execute(sql_insert_message, (''.join(str(uuid.uuid4()).split('-')), message_id, message_reveice_data, message_context))
-            cur.execute(sql_insert_qq_group_message_relationship, (''.join(str(uuid.uuid4()).split('-')), qq_id, group_id, message_id))
             db.commit()
+            print('qq_id:[' + qq_id + ']-qq昵称:[' + qq_nick_name + ']-分组id:[' + group_id + ']-分组名称:[' + group_name + ']-消息id:[' + message_id + ']-接收消息时间:[' + message_reveice_data + ']-消息内容:[' + message_context + ']')
 
     except Exception as e:
-        print(111)
+        print('数据库插入出现异常')
         print(e)
         db.rollback()
     finally:
